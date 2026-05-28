fa1fd7c2bacc88e9b49d49163b0f9585c59306a6
Mehemmed77 <mehemmedkazimzade54@gmail.com>
2026-05-28 21:56:56 +0400

    added print_heap() and a_free()

--- a_malloc.c
+++ a_malloc.c
@@ -9,8 +9,8 @@ struct block {
 
 typedef struct block block;
 
-block *head = NULL;
-block *tail = NULL;
+block* head = NULL;
+block* tail = NULL;
 
 char* HEAP_START = NULL;
 char* HEAP_END = NULL;
@@ -18,6 +18,18 @@ char* PTR = NULL;
 
 const int ALIGN_AMT = 8;
 const int INC_AMT = 4096;
+
+void print_heap() {
+    block* temp = head;
+    char* user_memory_address;
+
+    while(temp != NULL) {
+        user_memory_address = ((char*) temp) + sizeof(block);
+        printf("============\nBlock address:%p\nUser memory address:%p\nNext block address:%p\nSIZE: %zu\nFree:%u\n\n", 
+            temp, user_memory_address, temp->next, temp->size, temp->free);
+        temp = temp->next;
+    }
+}
 
 void init() {
     HEAP_START = sbrk(0);
@@ -38,30 +50,45 @@ void* a_malloc(size_t size) {
     size = align8(size);
 
     int available_space = (int) (HEAP_END - PTR);
-    
-    while(available_space < size) {
+
+    while(available_space < size + sizeof(block)) {
         sbrk(INC_AMT);
         HEAP_END += INC_AMT;
         available_space = HEAP_END - PTR;
     }
 
-    if (head == NULL) {
-        
-    }
+    block* temp = (block*) PTR;
+    temp->free = 0;
+    temp->size = size;
+    temp->next = NULL;
 
-    block new_block = {  };
+    if (head == NULL) head = temp;
 
-    int *curr = (int *) PTR;
-    PTR += size;
+    if (tail != NULL) tail->next = temp;
+
+    tail = temp;
+
+    void *curr = (void *) (PTR + sizeof(block));
+    PTR += sizeof(block) + size;
+
     return curr;
+}
+
+void a_free(void* ptr) {
+    block* block_ptr = (block *) ((char *) ptr - sizeof(block));
+    
+    block_ptr->free = 1;
 }
 
 int main() {
     int* a = (int *) a_malloc(10);
     int* b = (int *) a_malloc(20);
     int* c = (int *) a_malloc(20);
+    int* d = (int *) a_malloc(20);
+    
+    a_free(c);
 
-    printf("%p\n%p\n%p\n", a, b, c);
+    print_heap();
 
     return 0;
 }
